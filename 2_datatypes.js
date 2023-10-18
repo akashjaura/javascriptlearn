089745b2faf79cd433245156ff7b6ee6a93b0b04
akash jaura <84980791+akashjaura@users.noreply.github.com>
2023-10-18 10:26:28 +0000

    datatypes

---
+++ 2_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all js as newer version
+// alert(3+4) we are using node js not browser
+// use mdn for documentation
+let name="akash"
+let age=18
+let islog="true"
+//primitive datatypes
+//number
+//string
+//boolean
+//null
+//undefined
+//symbol
+//object
+console.log(typeof(13))
+console.log(typeof null)
---
+++ variables.js
@@ -0,0 +1,14 @@
+const accountId=123
+let accountEmail="anjncd@gmail.com"
+var acccountPassword="12345"
+accountCity="ohio"
+
+// not allowed accountId="23"
+
+/*
+prefer to not use var
+beacause of issue in block scope and functional block
+*/
+
+console.log(accountId);
+console.table([accountEmail,accountId,acccountPassword])
