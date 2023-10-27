f21c692f960d935a1fb099cad23d40b712c49b2f
akash jaura <84980791+akashjaura@users.noreply.github.com>
2023-10-27 06:36:03 +0000

    conversions

---
+++ 3_conversions.js
@@ -0,0 +1,35 @@
+// let score="33abc"
+// console.log(typeof score);
+// let valueINnumber=Number(score)
+// console.log(typeof valueINnumber);
+// console.log(valueINnumber)
+// let score="null"
+// console.log(typeof score);
+// let valueINnumber=Number(score)
+// console.log(typeof valueINnumber);
+// console.log(valueINnumber)
+// let score=undefined
+// console.log(typeof score);
+// let valueINnumber=Number(score)
+// console.log(typeof valueINnumber);
+// console.log(valueINnumber)
+// 1->true 0->false
+//""false
+//"hitesh"=true
+let isloggedin=""
+let booleais=Boolean(isloggedin)
+// console.log(booleais)
+//operations
+let v=3
+let negv=-v
+// console.log(negv)
+
+let str1="hello"
+let str2=" hitesh"
+// console.log(str1+str2)
+// console.log("1" + 2)
+// console.log(1+"2")
+// console.log(1+2+"2")
+// console.log("1"+2+2)
+let gamecounter=100
+console.log(++gamecounter + gamecounter++);
